7a82087da39cda0a355b58f26b7ee25aacbb0abc
saranawaz11 <saranawaz852@gmail.com>
2026-04-01 17:41:37 +0500

    hero section

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -1,10 +1,13 @@
+'use client'
 import { Badge } from '@/components/ui/badge'
-import { Sparkles } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { ArrowRight, Sparkles } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function HeroSection() {
     return (
-        <div className='flex items-center justify-center h-full'>
+        <section className='relative mx-auto  z-0 flex flex-col items-center justify-center py-16 sm:py-20 lg:py-28 transition-all animate-in lg:px-12 max-w-7xl'>
             <div className='group relative overflow-hidden rounded-full p-[2px] bg-linear-to-r from-rose-300 via-[#9E2A2B] to-[#540B0E] bg-size-[200%_100%] animate-gradient-x'>
                 <Badge
                     variant={'secondary'}
@@ -21,6 +24,17 @@ export default function HeroSection() {
                     </span>
                 </Badge>
             </div>
-        </div>
+            <h1 className='text-4xl max-w-3xl lg:text-5xl sm:text-2xl font-bold py-6 text-center'>Hours of reading, <span className='relative inline-block'>
+                <span className='z-10 relative px-2'>condensed</span>
+                <span className='absolute bg-rose-200/50 inset-0 -rotate-2 rounded-lg transform -skew-y-1' aria-hidden='true'></span>
+            </span> into moments of clarity.</h1>
+            <h2 className='text-lg sm:text-xl lg:text:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600'>Upload any PDF and let Briefly distil hours of reading into precise, intelligent summary - in seconds.</h2>
+            <Button variant={'link'} className='text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-6 shadow-lg transition-all duration-300 lg:mt-10 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline'>
+                <Link href='/#pricing' className='flex gap-2 items-center'>
+                    <span>Try Briefly</span>
+                    <ArrowRight size={32} className='h-9 w-9 animate-pulse' />
+                </Link>
+            </Button>
+        </section>
     )
 }
