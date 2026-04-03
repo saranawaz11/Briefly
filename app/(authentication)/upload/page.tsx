6f127926f14dc9076713da02b371ba70f3ce0278
saranawaz11 <saranawaz852@gmail.com>
2026-04-03 23:13:35 +0500

    header

---
+++ app/(authentication)/upload/page.tsx
@@ -0,0 +1,12 @@
+import UploadHeader from '@/app/components/UploadHeader'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='min-h-screen'>
+        <div className='mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8'>
+            <UploadHeader/>
+        </div>
+    </div>
+  )
+}
