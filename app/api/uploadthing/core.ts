baccc04e72f272cde7181179e5c9e2bc23aa4474
saranawaz11 <saranawaz852@gmail.com>
2026-04-04 19:53:12 +0500

    uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,28 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+    pdfUploader: f({
+        pdf: {
+            maxFileSize: "32MB",
+            maxFileCount: 1,
+        },
+    })
+        // Set permissions and file types for this FileRoute
+        .middleware(async ({ req }) => {
+            const user = await currentUser();
+            console.log("User:", user);
+            if (!user) throw new UploadThingError("Unauthorized");
+            return { userId: user.id };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("file url", file.ufsUrl);
+            return { uploadedBy: metadata.userId, file: file.url};
+        }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
--- app/components/home/upload/UploadForm.tsx
+++ app/components/home/upload/UploadForm.tsx
@@ -2,29 +2,54 @@
 import React from 'react'
 import FormInput from '@/app/components/home/upload/FormInput'
 import fileSchema from '@/lib/zod-schema';
+import { useUploadThing } from '@/utils/uploadthing';
 
 export default function UploadForm() {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+
+
+    const { startUpload, routeConfig } = useUploadThing("pdfUploader", {
+        onClientUploadComplete: () => {
+            console.log("uploaded successfully!");
+        },
+        onUploadError: (err) => {
+            console.log("error occurred while uploading", err);
+        },
+        onUploadBegin: ({ file }) => {
+            console.log("upload has begun for", file);
+        },
+    });
+
+
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('Submitted');
         const formData = new FormData(e.currentTarget);
         const file = formData.get('file') as File;
         console.log(file);
-        
+
         // validating the fields
-        const validatedFields = fileSchema.safeParse({file});
-        if(!validatedFields.success){
+        const validatedFields = fileSchema.safeParse({ file });
+        if (!validatedFields.success) {
             console.log(
                 validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid file'
             );
             return;
         }
+
+        //upload the file to uploading
+        const response = await startUpload([file]);
+        console.log("UPLOAD RESPONSE:", response);
+        if (!response) {
+            return;
+        }
     }
 
 
-  return (
-    <div className='flex flex-col gap-8 w-full max-w-2xl mx-auto'>
-        <FormInput onSubmit={handleSubmit}/>
-    </div>
-  )
+    return (
+        <div className='flex flex-col gap-8 w-full max-w-2xl mx-auto'>
+            <FormInput onSubmit={handleSubmit} />
+        </div>
+    )
 }
